15ce133f91659d155c59b7175ccc6669e96c9fd1
Nikolay Ilinov <Nikolay.Ilinov@x5.ru>
2023-01-06 10:34:27 +0700

    init

---
+++ android/app/src/main/kotlin/ru/ilinov/flutter_http_server/MainActivity.kt
@@ -0,0 +1,6 @@
+package ru.ilinov.flutter_http_server
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  void initState() {
+    startServer();
+    super.initState();
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  late HttpServer server;
+
+  startServer() async {
+    server = await HttpServer.bind(InternetAddress.loopbackIPv4, 8080);
+    print("Server running on IP : " + server.address.toString() + " On Port : " + server.port.toString());
+    await for (var request in server) {
+      print(request.connectionInfo?.remoteAddress);
+      request.response
+        ..headers.contentType = new ContentType("text", "plain", charset: "utf-8")
+        ..write('Hello, world')
+        ..close();
+    }
+  }
+
+  @override
+  void dispose() {
+    server.close(force: true);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
